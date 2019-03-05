4d15c839d77159709fa2084cd17826561455e7c0
Max Moro <massimiliano.moro@gmail.com>
2019-03-05 12:16:20 -0700

    V1 Max

---
+++ Scala/data_test.scala
@@ -0,0 +1,14 @@
+// Getting Started with DataFrames!
+
+// Most Important Link:
+// http://spark.apache.org/docs/latest/api/scala/index.html#org.apache.spark.sql.Dataset
+
+
+// Start a simple Spark Session
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession.builder().getOrCreate()
+
+// Create a DataFrame from Spark Session read csv
+// Technically known as class Dataset
+val df = spark.read.option("header","true").option("inferSchema","true").csv("data_rand.csv")
+df.head(6)
