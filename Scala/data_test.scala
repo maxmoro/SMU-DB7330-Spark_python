b21ac77322f653f1cb0160a6eb1db220c10aa778
Max Moro <massimiliano.moro@gmail.com>
2019-03-05 21:30:17 -0700

    first tentative with AWS Spark . Need to find a way to save the file on the web and read from scala

--- Scala/data_test.scala
+++ Scala/data_test.scala
@@ -10,8 +10,13 @@ val spark = SparkSession.builder().getOrCreate()
 
 // Create a DataFrame from Spark Session read csv
 // Technically known as class Dataset
-
+/*
+import org.apache.spark.SparkFiles
+spark.sparkContext.addFile("https://raw.githubusercontent.com/maxmoro/SMU-DB7330-Spark_python/master/data_rand_test.csv")
+spark.read.csv(SparkFiles.get("file.csv"))
+*/
 val df = spark.read.option("header","true").option("inferSchema","true").csv("data_rand.csv")
+//val df = spark.read.option("header","true").option("inferSchema","true").csv("https://raw.githubusercontent.com/maxmoro/SMU-DB7330-Spark_python/master/data_rand_test.csv")
 df.head(6)
 df.printSchema()
 df.groupBy("group1","group2").count().orderBy("group1","group2").show()
